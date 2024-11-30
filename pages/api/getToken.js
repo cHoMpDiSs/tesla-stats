db2dc503113ecd9fa2c70b81930468e6c4294ee7
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-11-30 13:27:42 -0800

    added param for token 2

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     const { TESLA_CLIENT_ID, TESLA_CLIENT_SECRET, TESLA_OAUTH_URL, TESLA_API_URL } = process.env;
 
     // Ensure required environment variables are set
-    if (!TESLA_CLIENT_ID || !TESLA_CLIENT_SECRET || !TESLA_OAUTH_URL) {
+    if (!TESLA_CLIENT_ID || !TESLA_CLIENT_SECRET || !TESLA_OAUTH_URL || TESLA_API_URL) {
         return res.status(500).json({ error: "Missing required environment variables" });
     }
 
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     params.append("grant_type", "client_credentials");
     params.append("client_id", TESLA_CLIENT_ID);
     params.append("client_secret", TESLA_CLIENT_SECRET);
-    params.append("audience", TESLA_API_URL);
+    params.append("audience", "https://fleet-api.prd.na.vn.cloud.tesla.com");
     params.append("scope", "openid vehicle_device_data vehicle_cmds profile_information offline_access user_data");
 
     try {
