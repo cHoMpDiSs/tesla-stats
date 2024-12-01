365e3d9e018c0a418028bd09ca761a5b95869c2c
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-11-30 21:00:42 -0800

    code params in token from getme 5

--- pages/api/getCar.js
+++ pages/api/getCar.js
@@ -1,14 +1,15 @@
 // api/getCar.js
 export default async function handler(req, res) {
   try {
-    const tokenRes = await fetch(`${process.env.BASE_URL}/api/getToken`);
-    const tokenData = await tokenRes.json();
+    // const tokenRes = await fetch(`${process.env.BASE_URL}/api/getToken`);
+    // const tokenData = await tokenRes.json();
     
-    if (!tokenRes.ok || !tokenData.access_token) {
-      return res.status(400).json({ error: "Failed to get token" });
-    }
+    // if (!tokenRes.ok || !tokenData.access_token) {
+    //   return res.status(400).json({ error: "Failed to get token" });
+    // }
 
-    const token = tokenData.access_token;
+    // const token = tokenData.access_token;
+    const token = "NA_ad1e3be60d0ee140a8e1d441d934b95701910105eb778017a9949c398ca3"
 
     const headers = { Authorization: `Bearer ${token}` };
 
