8b176646b6e763cc46c6b7683d97bdf06e52e642
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-12-02 20:25:02 -0800

    save getCar api 4

--- pages/api/getCar.js
+++ pages/api/getCar.js
@@ -5,24 +5,28 @@ export default async function handler(req, res) {
     // const tokenRes = await fetch(`${process.env.BASE_URL}/api/getToken`);
     // const tokenData = await tokenRes.json();
     
-    // if (!tokenRes.ok || !tokenData.access_token) {
-    //   return res.status(400).json({ error: "Failed to get token" });
-    // }
+    if (!tokenRes.ok || !tokenData.access_token) {
+      return res.status(400).json({ error: "Failed to get token" });
+    }
 
     // const token = tokenData.access_token;
     // const token = "NA_ad1e3be60d0ee140a8e1d441d934b95701910105eb778017a9949c398ca3"
 
-    const headers = { Authorization: `Bearer ${token}` };
 
-    const vehicleRes = await fetch(`${process.env.TESLA_API_URL}/api/1/vehicles`, { headers });
+
+    const vehicleRes = await fetch(`${process.env.TESLA_API_URL}/api/1/vehicles`,{
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  }
+  );
+  
     
-    
-    const vehicleData = await vehicleRes.json();
+
 
     if (!vehicleRes.ok || !vehicleData.response || vehicleData.response.length === 0) {
       return res.status(404).json({ error: "No vehicles found" });
     }
-
+    const vehicleData = await vehicleRes.json();
     const vehicleId = vehicleData.response[0].id;
     const carDataRes = await fetch(`${process.env.TESLA_API_URL}/api/1/vehicles/${vehicleId}/vehicle_data`, { headers });
     const carData = await carDataRes.json();
