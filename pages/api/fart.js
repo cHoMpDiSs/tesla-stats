7861e362f2dc7c1558bf733133259fd760f39551
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-12-10 13:14:59 -0800

    register app and fart api

--- pages/api/fart.js
+++ pages/api/fart.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   
     const cookies = parse(req.headers.cookie || '');  
     const accessToken = cookies.access_token;
+    console.log(accessToken, "ACCESS")
     const refreshToken = cookies.refresh_token;
     if (!accessToken || !refreshToken) {
       return res.status(400).json({ error: 'Missing access or refresh token' });
@@ -24,7 +25,7 @@ export default async function handler(req, res) {
           }),
         }
       );
-      
+      console.log(fartData, "FART DATA")
       if (fartData.ok) {
         console.log("Fart noise triggered successfully!");
       } else {
