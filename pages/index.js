ae7475a217e6e71a442c99b1443f839aac368208
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-12-24 12:53:51 -0800

    refresh token

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ import cyber from '../public/images/cyber.png'
 
 export default function Home() {
   return (
-    <div className="mx-10">
+    <div className="">
       <Head>
         <title>Tesla Stats</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col items-center justify-center py-10 ">
+      <main className="flex flex-col items-center justify-center my-10 ">
         <p className="font-poppins font-thin text-8xl mb-10 text-center">
           Tesla Stats
         </p>
-        <Image  src={cyber} alt="Cyber Truck" className="rounded-lg shadow-lg w-[45rem] xl:mt-4 lg:mt-2" />
+        <Image  src={cyber} alt="Cyber Truck" className="rounded-lg shadow-lg w-[24rem] md:w-[40rem] lg:w-[50rem] xl:w-[55rem] xl:mt-4 lg:mt-2 " />
         <div className="mt-1 grid lg:grid-cols-1 xl:grid-cols-2 gap-[0.625rem] md:grid-cols-1 md:gap-x-3 ">
           <BatteryStatus
           batteryLevel={20}
