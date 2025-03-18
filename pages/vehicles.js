d66a4611f47c8a4030a406944f29bf75de6933f4
Jordon Marchesano <jordonomarchesano@gmail.com>
2025-03-17 21:44:20 -0700

    added model Y photo

--- pages/vehicles.js
+++ pages/vehicles.js
@@ -2,9 +2,15 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { CircularProgress, Box, Typography } from "@mui/material";
+import {
+  Card,
+  CircularProgress,
+  CardContent,
+  Box,
+  Typography,
+  CardMedia,
+} from "@mui/material";
+import modelY from "../public/images/y.png";
 
 export default function VehicleData() {
   const [vehicleData, setVehicleData] = useState(null);
@@ -25,17 +31,14 @@ export default function VehicleData() {
         const data = await vehicleRes.json();
         if (vehicleRes.ok) {
           setVehicleData(data.response);
-        } else if (data.error = "Missing access or refresh token") {
-          refreshToken()
+        } else if ((data.error = "Missing access or refresh token")) {
+          refreshToken();
           // fetchVehicleData()
-        
-        } else{
+        } else {
           setError(data.error || "Failed to fetch vehicle data");
-       
         }
       } catch (err) {
         setError(err.message || "Failed to fetch vehicle data");
-      
       }
     }
 
@@ -56,23 +59,18 @@ export default function VehicleData() {
       if (response.ok) {
         console.log("Refresh token successful");
       } else {
-        if (data.error == "No refresh token provided"){
-          router.push("/auth")
+        if (data.error == "No refresh token provided") {
+          router.push("/auth");
         }
         console.error(data.error || "Failed to refresh token");
-  
       }
     } catch (err) {
-      
       console.error("Error refreshing token:", err);
     }
   };
 
-  if (
-    error === "login_required"
-  ) {
+  if (error === "login_required") {
     router.push("/auth");
-
   } else if (error) {
     return (
       <div className="flex items-center justify-center h-screen bg-red-50">
@@ -86,7 +84,6 @@ export default function VehicleData() {
       </div>
     );
   }
-  
 
   async function wakeUpAndPoll(vin) {
     setPolling(true);
@@ -144,85 +141,96 @@ export default function VehicleData() {
   const goToVehiclePage = (id, vin) => {
     router.push(`/vehicle?id=${id}&vin=${vin}`);
   };
+console.log(modelY)
+  if (polling) {
+    return (
+      <div className="my-auto text-center">
+        <CircularProgress />
+      </div>
+    );
 
-if(polling){
-return<div className="my-auto text-center">
-<CircularProgress/>
-</div> 
-}
-
-  else if(vehicleData){ 
-  return (
-    <div className="min-h-screen  py-8 px-4 ">
-      <h1 className="text-4xl  text-center mb-8 ">Fleet</h1>
-      <div className="flex flex-col items-center">
-        <div
-          className={`grid gap-6 ${
-            vehicleData.length === 1
-              ? "grid-cols-1"
-              : vehicleData.length === 2
-                ? "grid-cols-2"
-                : "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-          }`}
-        >
-          {vehicleData.map((vehicle) => (
-            <Card
-              key={vehicle.id}
-              className="shadow-lg rounded-lg  hover:shadow-2xl transition-all duration-300 border border-gray-200"
-            >
-              <CardContent className="p-6">
-                <Box className="mb-4">
-                  <Typography variant="h6" className="font-bold ">
-                    Model: {vehicle.vin[3]}
-                  </Typography>
-                  <Typography variant="body2" className="">
-                    <strong>ID:</strong> {vehicle.id}
-                  </Typography>
-                  <Typography variant="body2" className="">
-                    <strong>VIN:</strong> {vehicle.vin}
-                  </Typography>
-                  <Typography variant="body2" className="">
-                    <strong>Color:</strong> {vehicle.color || "Midnight Silver"}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    className={`font-medium ${
-                      vehicle.state === "online"
-                        ? "text-green-600"
-                        : "text-red-500"
-                    }`}
-                  >
-                    <strong>State:</strong> {vehicle.state}
-                  </Typography>
-                </Box>
-              </CardContent>
-              <Box className="p-4 flex justify-center">
-                {vehicle.state === "offline" || vehicle.state === "asleep" ? (
-                  polling ? (
-                    <CircularProgress size={40} color="primary" />
+  } 
+  else if (vehicleData) {
+    return (
+      <div className="min-h-screen  py-8 px-4 ">
+        <h1 className="text-4xl  text-center mb-8 ">Fleet</h1>
+        <div className="flex flex-col items-center">
+          <div
+            className={`grid gap-6 ${
+              vehicleData.length === 1
+                ? "grid-cols-1"
+                : vehicleData.length === 2
+                  ? "grid-cols-2"
+                  : "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+            }`}
+          >
+            {vehicleData.map((vehicle) => (
+              <Card
+                key={vehicle.id}
+                className="max-w-[30rem] shadow-lg rounded-lg  hover:shadow-2xl transition-all duration-300 border border-gray-200"
+              >
+                    <Typography variant="h6" className="font-bold text-center ">
+                      Model {vehicle.vin[3]}
+                    </Typography>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={modelY.src}
+                  alt="Model Y"
+                />
+                <CardContent className="p-6">
+                  <Box className="mb-4">
+                
+                    <Typography variant="body2" className="">
+                      <strong>ID:</strong> {vehicle.id}
+                    </Typography>
+                    <Typography variant="body2" className="">
+                      <strong>VIN:</strong> {vehicle.vin}
+                    </Typography>
+                    <Typography variant="body2" className="">
+                      <strong>Color:</strong>{" "}
+                      {vehicle.color || "Midnight Silver"}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      className={`font-medium ${
+                        vehicle.state === "online"
+                          ? "text-green-600"
+                          : "text-red-500"
+                      }`}
+                    >
+                      <strong>State:</strong> {vehicle.state}
+                    </Typography>
+                  </Box>
+                </CardContent>
+                <Box className="p-4 flex justify-center">
+                  {vehicle.state === "offline" || vehicle.state === "asleep" ? (
+                    polling ? (
+                      <CircularProgress size={40} color="primary" />
+                    ) : (
+                      <Button
+                        variant="contained"
+                        onClick={() => wakeUpAndPoll(vehicle.vin)}
+                        className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded"
+                      >
+                        Wake Up
+                      </Button>
+                    )
                   ) : (
                     <Button
                       variant="contained"
-                      onClick={() => wakeUpAndPoll(vehicle.vin)}
-                      className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded"
+                      onClick={() => goToVehiclePage(vehicle.id, vehicle.vin)}
+                      className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded"
                     >
-                      Wake Up
+                      View {vehicle.display_name} Data
                     </Button>
-                  )
-                ) : (
-                  <Button
-                    variant="contained"
-                    onClick={() => goToVehiclePage(vehicle.id, vehicle.vin)}
-                    className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded"
-                  >
-                    View {vehicle.display_name} Data
-                  </Button>
-                )}
-              </Box>
-            </Card>
-          ))}
+                  )}
+                </Box>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}}
+    );
+  }
+}
